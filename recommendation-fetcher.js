7abfc8058b9d9a8df2b08b639df23543e030a983
emtseng <etseng42@gmail.com>
2017-07-06 19:32:32 -0400

    refactor: simplifies getRecommendations

--- recommendation-fetcher.js
+++ recommendation-fetcher.js
@@ -1,107 +1,99 @@
-// recommendation-fetcher.js
+module.exports = {
 
-var RecommendationFetcher;
-
-RecommendationFetcher = {
-
-  fetchRecommendation: function(apiKey, site, search, fetch){
+  fetchRecommendation: function (apiKey, site, search, fetch) {
     var url = 'https://www.googleapis.com/customsearch/v1?q=' +
-    search + ' &cx=013013877924597244999%3Atbq0ixuctim&dateRestrict=m[7]&siteSearch=' +
-    site + '&key=' + apiKey;
+      search + ' &cx=013013877924597244999%3Atbq0ixuctim&dateRestrict=m[7]&siteSearch=' +
+      site + '&key=' + apiKey;
     return fetch(url);
   },
 
-  fetchRecommendations: function(apiKey, sites, search, fetch){
+  fetchRecommendations: function (apiKey, sites, search, fetch) {
     var recommendationPromises = [];
-    for (var i=0; i < sites.length; i++){
+    for (var i = 0; i < sites.length; i++) {
       recommendationPromises.push(this.fetchRecommendation(apiKey, sites[i], search, fetch));
     }
     return recommendationPromises;
   },
 
-  getRecommendations: function(recommendationPromises){
-    var recommendations;
-    $.when.apply($, recommendationPromises).then(function(){
-      results = Array.prototype.slice.call(arguments);
-    })
-    return recommendations;
+  getRecommendations: function (recommendationPromises) {
+    return $.when.apply($, recommendationPromises).then(results => results)
   },
 
-  getTransformedRecommendations: function(recommendations, sites){
+  getTransformedRecommendations: function (recommendations, sites) {
     var transformedRecommendations = [];
     var transformRecommendation;
-    for (var i=0; i < recommendations.length; i++){
+    for (var i = 0; i < recommendations.length; i++) {
       transformedRecommendation = this.transformRecommendation(recommendation[i], sites[i]);
       transformedRecommendations.push(transformRecommendation);
     }
     return transformedRecommendations;
   },
 
-  extractHeadline: function(recommendation, publisher){
-    if (publisher === "nationalreview.com"){
+  extractHeadline: function (recommendation, publisher) {
+    if (publisher === "nationalreview.com") {
       var headline = recommendation.pagemap.article[0].headline;
       return headline !== undefined ? headline : recommendation.title;
-      }
-      return recommendation.title;
-    },
+    }
+    return recommendation.title;
+  },
 
-  extractLink: function(recommendation){
+  extractLink: function (recommendation) {
     return recommendation.link;
   },
 
-  formatDate: function(date){
+  formatDate: function (date) {
     var cutoffIndex = 16;
     return date != undefined ? new Date(date).toUTCString().slice(0, cutoffIndex) : "";
   },
 
-  handlePropertyLookup: function(collection, field){
+  handlePropertyLookup: function (collection, field) {
     try {
       return collection[0][field];
     }
-    catch(err){
+    catch (err) {
       return undefined;
     }
   },
 
-  extractDate: function(recommendation, publisher){
+  extractDate: function (recommendation, publisher) {
     var pagemap = recommendation.pagemap;
     var date;
-    if (publisher === "foxnews.com"){
+    if (publisher === "foxnews.com") {
       date = this.handlePropertyLookup(pagemap.metatags, 'dc.date');
-    } else if (publisher === "nationalreview.com"){
+    } else if (publisher === "nationalreview.com") {
       date = this.handlePropertyLookup(pagemap.article, 'datepublished');
-    } else if (publisher === "nypost.com"){
+    } else if (publisher === "nypost.com") {
       date = this.handlePropertyLookup(pagemap.metatags, 'article:published_time');
-    } else if (publisher === "wsj.com"){
+    } else if (publisher === "wsj.com") {
       date = this.handlePropertyLookup(pagemap.webpage, 'datecreated');
-    } else if (publisher === "theatlantic.com"){
+    } else if (publisher === "theatlantic.com") {
       date = this.handlePropertyLookup(pagemap.newsarticle, 'datepublished');
     }
     return this.formatDate(date);
   },
 
-  extractDescription: function(recommendation, publisher){
+  extractDescription: function (recommendation, publisher) {
     var pagemap = recommendation.pagemap;
     var description;
-    if (publisher === "foxnews.com"){
+    if (publisher === "foxnews.com") {
       description = this.handlePropertyLookup(pagemap.metatags, 'dc.description');
-    } else if (publisher === "nationalreview.com"){
+    } else if (publisher === "nationalreview.com") {
       description = this.handlePropertyLookup(pagemap.article, 'articlebody');
-    } else if (publisher == "nypost.com"){
+    } else if (publisher == "nypost.com") {
       description = this.handlePropertyLookup(pagemap.metatags, 'og:description');
-    } else if (publisher === "wsj.com"){
+    } else if (publisher === "wsj.com") {
       description = this.handlePropertyLookup(pagemap.webpage, 'description');
-    } else if (publisher === "theatlantic.com"){
+    } else if (publisher === "theatlantic.com") {
       description = this.handlePropertyLookup(pagemap.newsarticle, 'description');
-    } else if (publisher === "vice.com"){
+    } else if (publisher === "vice.com") {
       description = this.handlePropertyLookup(pagemap.metatags, 'og:description');
-    } else if (publisher === "slate.com"){
+    } else if (publisher === "slate.com") {
       description = this.handlePropertyLookup(pagemap.metatags, 'og:description');
     }
     return description;
   },
 
-  transformRecommendation: function(recommendation, publisher){
+  transformRecommendation: function (recommendation, publisher) {
     return {
       headline: this.extractHeadline(recommendation, publisher),
       link: this.extractLink(recommendation, publisher),
@@ -110,6 +102,5 @@ RecommendationFetcher = {
     };
   }
 
-  };
+};
 
-module.exports = RecommendationFetcher;
