c54f490ec10975d9a486f421172b8e535a96845d
emtseng <etseng42@gmail.com>
2017-08-01 21:54:23 -0400

    makes font important for popover

--- src/btm-embed-icon.js
+++ src/btm-embed-icon.js
@@ -35,7 +35,7 @@ $(function () {
 				, slug = getSlug(href)
 				, $btm_button = $(`<a href="javascript:void(0);"><img src=${btmImg} style="height: 20px; width: 20px; vertical-align: middle; margin-left: 0.1em"></a>`)
 				, popoverTemplate = getPopoverHtml(slug)
-				, loading = `<div class="btm-popover" id="btm-popover-body-${slug}"><div id="btm-loading-${slug}"><p>Loading...</p></div></div>`;
+				, loading = `<div id="btm-popover-body-${slug}"><div id="btm-loading-${slug}"><p>Loading...</p></div></div>`;
 
 			$btm_button.popover({
 				trigger: "click",
