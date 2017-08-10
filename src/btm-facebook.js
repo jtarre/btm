77285816e0ee9b88d174ed44d48ecf7c9d60996a
Anish Dalal <anishdalal1@gmail.com>
2017-08-10 08:40:20 -0400

    fixed embed icon issue on facebook and made helper function to get publisher from url

--- src/btm-facebook.js
+++ src/btm-facebook.js
@@ -2,7 +2,7 @@ import uniqBy from 'lodash.uniqby'
 
 import { getPopoverHtml, getBTMIcon, getLoading, getPopoverTitle } from './helpers/inline-elements'
 
-import { spectrumSites, siteTitles, getSlug, createPopup, siteSearches } from './helpers/site-constants'
+import { spectrumSites, siteTitles, getSlug, createPopup, siteSearches, getPublisher } from './helpers/site-constants'
 
 import { checkIsArticle, checkLinkSection, checkIsProperSource } from './helpers/getLinks-helpers'
 
@@ -29,9 +29,10 @@ $(() => {
 			const $element = $(element)
 				, href = $element.attr('href')
 				, slug = getSlug(href)
-				, $newsfeedPost = $element.closest('.fbUserContent').first()
+				, $newsfeedPost = $element.closest('.fbUserPost').first()
 				, $postText = $newsfeedPost.find('.userContent')
 				, $btmButton = getBTMIcon(btmImg)
+				, publisher = getPublisher(href)
 
 			$btmButton.popover({
 				trigger: "click",
@@ -63,7 +64,7 @@ $(() => {
 
 			function initPopover() {
 				$('.btm-close').on('click', () => { $btmButton.popover('hide') });
-				Promise.all(siteSearches(spectrumSites['nytimes.com'], slug)) // hard-coded for NYT only
+				Promise.all(siteSearches(spectrumSites[publisher], slug)) // hard-coded for NYT only
 					.then(results => {
 						$(`#btm-loading-${slug}`).hide();
 						$(`#btm-popover-body-${slug}`).after(createPopup(results, slug));
