77c1b6ca32154aea9748e7b4e2b1537a4c0dd9b7
emtseng <etseng42@gmail.com>
2017-08-13 22:06:30 -0400

    fixes section page bug

--- src/helpers/inline-elements.js
+++ src/helpers/inline-elements.js
@@ -1,4 +1,4 @@
-export const getBTMIcon = (btmImg) => ($(`<a href="javascript:void(0);"><img src=${btmImg} style="height: 20px; width: 20px; vertical-align: middle; margin-left: 0.1em"></a>`))
+export const getBTMIcon = (btmImg) => ($(`<a href="javascript:void(0);" class="btm-icon"><img src=${btmImg} style="height: 20px; width: 20px; vertical-align: middle; margin-left: 0.1em"></a>`))
 
 export const getLoading = (slug) => (`<div id="btm-popover-body-${slug}"><div id="btm-loading-${slug}" style="text-align: center;"><p>Loading...</p></div></div>`)
 
