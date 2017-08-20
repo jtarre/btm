32a257f12e2854a176449ced7793598f3fb3423a
Anish Dalal <anishdalal1@gmail.com>
2017-08-19 22:00:23 -0400

    Moved logic for init page hover to separate module

--- js/btm.js
+++ js/btm.js
@@ -1,6 +1,8 @@
 import { initPageHover, isValidArticle } from './helpers/article-page'
 import { isSectionPage, embedIcons } from './helpers/section-page'
 
+import { initPageHover } from './helpers/article-page'
+
 $(() => {
 	const url = window.location.href
 	$('head').append("<style>@import url('https://fonts.googleapis.com/css?family=Josefin+Sans');</style>")
@@ -8,5 +10,4 @@ $(() => {
 		setTimeout(embedIcons(url), 3000)
 	} else if (isValidArticle(url)) {
 		initPageHover(url)
-	}
 })
